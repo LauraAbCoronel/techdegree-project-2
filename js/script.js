421b280010863c672901fd2d789661792826d292
LauraAbCoronel <68669949+LauraAbCoronel@users.noreply.github.com>
2021-01-15 20:41:01 -0500

    Added page buttons
    They don't do anything at the moment

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const studentList = document.querySelectorAll('.student-item');
 const maxDisplay = 10;
 
 // showPage function takes in the list of student and the page number
-// The function will ten loop through the list and hide the students that are not within the index of the page number
+// The function will then loop through the list and hide the students that are not within the index of the page number
 function showPage(studentList,page) {
    for (let i = 0; i < studentList.length; i++) {
       const startIndex = (maxDisplay*page)-maxDisplay;
@@ -25,8 +25,29 @@ function showPage(studentList,page) {
    functionality to the pagination buttons.
 ***/
 
+function appendPageLinks(studentList) {
+   const pages = Math.ceil(studentList.length/maxDisplay);
+   const divPage = document.querySelector('.page');
+   const div = document.createElement('div');
+   div.className = "pagination";
+   const ul = document.createElement('ul');
 
+   for (let i = 0; i < pages; i++) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.textContent = i+1;
+      if (i===0) {
+         a.className = 'active';
+      }
+      a.href = '#';
+      li.appendChild(a);
+      ul.appendChild(li);
+   }
+   div.appendChild(ul);
+   divPage.appendChild(div);
+}
 
+appendPageLinks(studentList);
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
