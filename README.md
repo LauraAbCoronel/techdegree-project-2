03fcb523a89641785b86dbb127312a9f21b06779
LauraAbCoronel <68669949+LauraAbCoronel@users.noreply.github.com>
2021-01-15 20:08:08 -0500

    Created showPage Function

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # techdegree-project-2
  List Pagination and Filtering Project
-In this project we take a list of student names and break them up into groups of 10
+In this project we take a list of student names in the HTML and break them up into managable groups of 10.
+
+I tried for the Exceeds Expectation grade by implementing a search bar and have the results display in groups of 10.
--- js/script.js
+++ js/script.js
@@ -1,41 +1,23 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+// Setting the global variables.
+// studentList contains a list of all the elements with the class "student-item"
+// maxDisplay is the number of students to show on each page.
+const studentList = document.querySelectorAll('.student-item');
+const maxDisplay = 10;
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
+// showPage function takes in the list of student and the page number
+// The function will ten loop through the list and hide the students that are not within the index of the page number
+function showPage(studentList,page) {
+   for (let i = 0; i < studentList.length; i++) {
+      const startIndex = (maxDisplay*page)-maxDisplay;
+      const endIndex = maxDisplay*page;
+      if (i>=startIndex && i<endIndex) {
+         studentList[i].style.display = '';
+      } else {
+         studentList[i].style.display = "none";
+      }
+   }
+}
 
 
 /*** 
